07eb5d1d718faba0178178e2a813b284bf42caf4
IanRMarques <ian.romancini.marques.com>
2022-08-30 10:27:21 -0300

    Projete atualizado

--- projete.js
+++ projete.js
@@ -1,15 +1,29 @@
 function subir(){
-    alert(subindo);
+    console.log("subindo"); 
+    alert("Subindo");
 }
 
 function descer(){
+    console.log("descendo");
+    alert("Descendo");
 }
 
 function acelerar1(){
+    alert("1x");
 }
 
 function acelerar2(){
+    alert("2X");
 }
 
 function acelerar3(){
+    alert("3X");
 }
+
+function conectarbluetooth(){
+    alert("Conectando");
+    navigator.bluetooth.requestDevice;
+}
+
+btn.addEventListener('')
+btn.addEventListener('')
